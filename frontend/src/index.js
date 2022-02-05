80be4f93ba1df32e1ebde144c60109fdeb14f57a
Faizan Qureshi <qureshi.learner@gmail.com>
2022-02-05 06:51:50 +0500

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import configAxios from './plugins/axios';
-import SnackBarProvider from './components/Snackbar';
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import configAxios from './plugins/axios'
+import SnackBarProvider from './components/Snackbar'
 
 configAxios()
 
@@ -18,9 +18,9 @@ ReactDOM.render(
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
